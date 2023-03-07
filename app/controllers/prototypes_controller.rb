4e217c00c109567a52dbd95cb00913845b513180
ayukoxxx <62144815+ayukoxxx@users.noreply.github.com>
2023-03-07 21:50:45 +0900

    prototype作成から削除

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,49 @@
 class PrototypesController < ApplicationController
   def index
-    @user = User.new
+    @prototypes = Prototype.all
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      @prototype = Prototype.new(prototype_params) 
+      render 'new'
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+    redirect_to  prototype_path(prototype.id)
+    else
+      @prototype = Prototype.find(params[:id])
+    render 'edit'
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
 end
